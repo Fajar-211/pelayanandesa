db47522074ee81b04632b57d8ac9863385c41904
Fajar-211 <21102183@ittelkom-pwt.ac.id>
2024-08-31 02:26:11 +0700

    Fajar: User (navigasi & edit)

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -6,48 +6,53 @@
   </x-slot> --}}
   <div class="drawer lg:drawer-open">
     <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-    <div class="drawer-content flex flex-col items-center justify-center bg-[#DEF2F1] relative">
+    <div class="drawer-content flex flex-col items-center justify-start bg-[#DEF2F1]">
       <!-- Page content here -->
 
-      <!-- KOTAK WARNA PUTIH -->
-      <div class="w-full h-24 bg-white items-end justify-end absolute top-0">
-        <ul class="menu menu-vertical lg:menu-horizontal rounded-box items-center text-black mt-12 mr-8 mb-12">
-          <li>Hallo, {{$nama}}</li>
-          <li>
-            <details class="dropdown">
-              <summary>
+      <!-- KOTAK WARNA ATAS -->
+      <div class="navbar rounded-box mt-10">
+        <div class="flex flex-1 justify-end px-2">
+          <div class="flex items-stretch">
+            <h2 class="my-4 mr-4 text-black" id="user">Hallo {{$nama}} </h2>
+            <!-- Info Profil -->
+            <div class="dropdown dropdown-end mr-4">
+              <div tabindex="0" role="button" class="btn btn-ghost rounded-btn">
                 <div class="avatar">
-                  <div class="w-8 rounded-full">
+                  <div class="w-12 rounded-full">
                     <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
                   </div>
                 </div>
-              </summary>
-              <ul class="menu dropdown-content bg-white rounded-box z-[1] w-52 p-2 shadow">
-                <li><a>Info profile</a></li>
-                <li><a>Log out</a></li>
+              </div>
+              <ul tabindex="0" class="menu dropdown-content bg-white rounded-box z-[1] mt-4 w-52 p-2 shadow">
+                <li><a class="text-black" id="showInfo" onclick="showInfo()">Info Profil</a></li>
+                <li><a class="text-red">Log Out</a></li>
               </ul>
-            </details>
-          </li>
-          <li>
-            <details class="dropdown">
-              <summary>
+            </div>
+            <!-- Notifikasi -->
+            <div class="dropdown dropdown-end">
+              <div tabindex="0" role="button" class="btn btn-ghost rounded-btn">
                 <div class="avatar">
-                  <div class="w-8 rounded-full">
+                  <div class="w-12 rounded-full">
                     <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
                   </div>
                 </div>
-              </summary>
-              <ul class="menu dropdown-content bg-white rounded-box z-[1] w-52 p-2 shadow">
-                <li><a>Info profile</a></li>
-                <li><a>Log out</a></li>
+              </div>
+              <ul tabindex="0" class="menu dropdown-content bg-white rounded-box z-[1] mt-4 w-52 p-2 shadow">
+                <li class="text-center text-black">Notifikasi</li>
+                <li></li>
+                <li><a>Notifikasi 1</a></li>
+                <li><a>Notifikasi 2</a></li>
+                <li><a>Notifikasi 3</a></li>
+                <li><a>Notifikasi 4</a></li>
+                <li><a>Notifikasi 5</a></li>
               </ul>
-            </details>
-          </li>
-        </ul>
+            </div>
+          </div>
+        </div>
       </div>
 
-      <!-- ISI KONTEN (DI BAWAH KOTAK PUTIH) -->
-      <div class="w-5/6 hidden" id="DivSurat"><!-- Surat Pengantar -->
+      <!-- ISI KONTEN (DI BAWAH) -->
+      <div class="w-5/6 hidden mt-10" id="DivSurat"><!-- Surat Pengantar -->
         <div class="card text-primary-content bg-white">
           <div class="card-body">
             <h2 class="card-title">ISI FORM</h2>
@@ -78,11 +83,11 @@
         </div>
       </div>
 
-      <div class="w-5/6 hidden" id="DivKas"><!-- Kas Warga -->
+      <div class="w-5/6 hidden mt-10" id="DivKas"><!-- Kas Warga -->
         <h2>Hallo ini kotak kas warga</h2>
       </div>
-      
-      <div class="w-5/6 hidden" id="DivLapor"><!-- Wajib Lapor -->
+
+      <div class="w-5/6 hidden mt-12" id="DivLapor"><!-- Wajib Lapor -->
         <div class="card text-primary-content bg-white">
           <div class="card-body">
             <h2 class="card-title">ISI FORM</h2>
@@ -109,19 +114,88 @@
         </div>
       </div>
 
+      <div class="w-5/6 hidden" id="DivInfo"><!-- Info Porfile -->
+        <div class="card text-primary-content">
+          <div class="card-body">
+            <div class="avatar justify-center">
+              <div class="w-24 rounded-full">
+                <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+              </div>
+            </div>
+            <h2 class="text-center">{{$nama}}</h2>
+            <table>
+              <tr>
+                <td class="text-green-500">Nomor KK</td>
+              </tr>
+              <tr>
+                <td class="text-stone-400">example</td>
+              </tr>
+              <tr></tr>
+              <tr>
+                <td class="text-green-500">NIK</td>
+              </tr>
+              <tr>
+                <td class="text-stone-400">example</td>
+              </tr>
+              <tr></tr>
+              <tr>
+                <td class="text-green-500">Jenis Kelamin</td>
+              </tr>
+              <tr>
+                <td class="text-stone-400">example</td>
+              </tr>
+              <tr></tr>
+              <tr>
+                <td class="text-green-500">Tanggal Lahir</td>
+              </tr>
+              <tr>
+                <td class="text-stone-400">example</td>
+              </tr>
+              <tr></tr>
+              <tr>
+                <td class="text-green-500">Email</td>
+              </tr>
+              <tr>
+                <td class="text-stone-400">example</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+        <div class="card-actions justify-end">
+          <button class="btn btn-accent text-white" id="showEdit" onclick="showEdit()">Edit</button>
+        </div>
+      </div>
+
+      <div class="w-5/6 hidden" id="DivEdit"><!-- Edit Porfile -->
+        <div class="card text-primary-content">
+          <div class="card-body">
+            <div class="avatar justify-center">
+              <div class="w-24 rounded-full">
+                <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+              </div>
+            </div>
+            <h2 class="text-center">EDIT PROFIL</h2>
+          </div>
+        </div>
+        <div class="card-actions justify-end">
+          <button class="btn btn-secondary text-white">Batal</button>
+          <button class="btn btn-accent text-white">Simpan</button>
+        </div>
+      </div>
+
 
 
 
       <!-- KOTAK MENU SEBELAH -->
       <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">
-        Open drawer
+        Buka Tab
       </label>
     </div>
     <div class="drawer-side">
       <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
       <ul class="menu bg-[#3AAFA9] text-white min-h-full w-80 p-4">
         <!-- Sidebar content here -->
-        <h1 class="text-center">JUDUL</h1>
+        <h1 class="text-center my-10 text-2xl">RT 3 RW 1</h1>
         <li><a id="showSurat" onclick="showSurat()">Surat pengantar</a></li>
         <li><a id="showKas" onclick="showKas()">Kas warga</a></li>
         <li><a id="showLapor" onclick="showLapor()">Wajib lapor</a></li>
@@ -133,16 +207,42 @@
       document.getElementById("DivSurat").style.display = "block";
       document.getElementById("DivKas").style.display = "none";
       document.getElementById("DivLapor").style.display = "none";
+      document.getElementById("DivInfo").style.display = "none";
+      document.getElementById("user").style.display = "block";
+      document.getElementById("DivEdit").style.display = "none";
     }
     function showKas() {
       document.getElementById("DivSurat").style.display = "none";
       document.getElementById("DivKas").style.display = "block";
       document.getElementById("DivLapor").style.display = "none";
+      document.getElementById("DivInfo").style.display = "none";
+      document.getElementById("user").style.display = "block";
+      document.getElementById("DivEdit").style.display = "none";
     }
     function showLapor() {
       document.getElementById("DivSurat").style.display = "none";
       document.getElementById("DivKas").style.display = "none";
       document.getElementById("DivLapor").style.display = "block";
+      document.getElementById("DivInfo").style.display = "none";
+      document.getElementById("user").style.display = "block";
+      document.getElementById("DivEdit").style.display = "none";
     }
+    function showInfo() {
+      document.getElementById("DivSurat").style.display = "none";
+      document.getElementById("DivKas").style.display = "none";
+      document.getElementById("DivLapor").style.display = "none";
+      document.getElementById("DivInfo").style.display = "block";
+      document.getElementById("user").style.display = "none";
+      document.getElementById("DivEdit").style.display = "none";
+    }
+    function showEdit() {
+      document.getElementById("DivSurat").style.display = "none";
+      document.getElementById("DivKas").style.display = "none";
+      document.getElementById("DivLapor").style.display = "none";
+      document.getElementById("DivInfo").style.display = "none";
+      document.getElementById("user").style.display = "none";
+      document.getElementById("DivEdit").style.display = "block";
+    }
+
   </script>
 </x-app-layout>
