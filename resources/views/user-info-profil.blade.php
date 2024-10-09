d14ea486d64330d9db480916b9a6a5a8badeec16
Mayopi <cyade007@gmail.com>
2024-10-09 13:31:27 +0700

    fix: foto profil path for displaying view

--- resources/views/user-info-profil.blade.php
+++ resources/views/user-info-profil.blade.php
@@ -83,9 +83,9 @@
             <div class="w-5/6 hidden" id="DivInfo"><!-- Info Porfile -->
                 <div class="card text-primary-content">
                     <div class="card-body">
-                        <div class="avatar justify-center">
+                        <div class="avatar justify-center cursor-pointer">
                             <div class="w-24 rounded-full">
-                                <img src="{{ asset('storage/foto_profil/' . $dataWarga->foto_profil) }}" alt="Foto Profil" style="width: 150px; height: 150px;" />
+                                <img src="{{ asset('storage/' . $dataWarga->foto_profil) }}" alt="Foto Profil" style="width: 150px; height: 150px;" />
                             </div>
                         </div>
                         <h2 class="text-center"></h2>
